701c134b2a14bacad69eb0543750b0e0e6bb25e2
RoshanDasan <edroshan2001@gmail.com>
2024-02-22 11:31:11 +0530

    test: vercel issue

--- src/app/components/utility-components/container/Container.tsx
+++ src/app/components/utility-components/container/Container.tsx
@@ -3,33 +3,39 @@ import { DEFAULT_COLORS } from '@/app/utils/colors';
 import { ComponentProps } from '@/app/utils/types';
 import { forwardRef } from 'react';
 
-const Container = forwardRef<HTMLDivElement, ComponentProps & BoxProps>(({
-    children,
-    width,
-    maxWidth,
-    background,
-    borderRadius = "15px",
-    padding = "10px",
-    fullWidth,
-  
-    ...otherProps
-}, ref) => {
+const Container = forwardRef<HTMLDivElement, ComponentProps & BoxProps>(
+  function Container(
+    {
+      children,
+      width,
+      maxWidth,
+      background,
+      borderRadius = "15px",
+      padding = "10px",
+      fullWidth,
+      ...otherProps
+    },
+    ref
+  ) {
     return (
-        <Box
-            sx={{
-                border: `1px solid ${DEFAULT_COLORS.Light}`,
-                borderRadius,
-                padding,
-                background,
-                width: fullWidth ? '100%' : width,
-                maxWidth,
-            }}
-            ref={ref}
-            {...otherProps}
-        >
-            {children}
-        </Box>
+      <Box
+        sx={{
+          border: `1px solid ${DEFAULT_COLORS.Light}`,
+          borderRadius,
+          padding,
+          background,
+          width: fullWidth ? '100%' : width,
+          maxWidth,
+        }}
+        ref={ref}
+        {...otherProps}
+      >
+        {children}
+      </Box>
     );
-});
+  }
+);
+
+Container.displayName = 'Container';
 
 export default Container;
