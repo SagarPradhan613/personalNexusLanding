b81197844e93ab8cae990dda0fad53b1e08ff18e
SagarPradhan613 <karanpradhan42@gmail.com>
2024-02-26 10:38:27 +0530

    added glow effect on btn

--- src/app/components/utility-components/container/Container.tsx
+++ src/app/components/utility-components/container/Container.tsx
@@ -2,6 +2,7 @@ import { Box, BoxProps } from '@mui/material';
 import { DEFAULT_COLORS } from '@/app/utils/colors';
 import { ComponentProps } from '@/app/utils/types';
 import { forwardRef } from 'react';
+import './container.css';
 
 const Container = forwardRef<HTMLDivElement, ComponentProps & BoxProps>(
   function Container(
@@ -19,6 +20,7 @@ const Container = forwardRef<HTMLDivElement, ComponentProps & BoxProps>(
   ) {
     return (
       <Box
+        className="test-hover"
         sx={{
           border: `1px solid ${DEFAULT_COLORS.Light}`,
           borderRadius,
--- src/app/view/landing/LandingSecondary.tsx
+++ src/app/view/landing/LandingSecondary.tsx
@@ -9,7 +9,39 @@ import useIsMobile from "@/app/hooks/useIsMobile"
 import { DEFAULT_COLORS } from "@/app/utils/colors"
 import { IMAGE_COLLECTIONS } from "@/app/utils/images"
 import { RefObject } from "react"
+import styled from "styled-components"
 
+const MainRelativeContainer = styled.div`
+    position:relative;
+`
+
+const LeftMaskContainer = styled.div`
+    position:absolute;
+    top:-35rem;
+    left:0;
+    z-index:0;
+    @media (max-width: 1024px) {
+        display:none;
+    }
+
+    @media (min-width: 1024px) {
+        display:block;
+      }
+`
+
+const RightMaskContainer = styled.div`
+    position:absolute;
+    top:-65rem;
+    right:0;
+    z-index:50;
+    @media (max-width: 1024px) {
+        display:none;
+    }
+
+    @media (min-width: 1024px) {
+        display:block;
+      }
+`
 
 const LandingSecondary = () => {
     const boxRef = useIntersectionAnimation() as RefObject<HTMLDivElement>;
@@ -19,22 +51,31 @@ const LandingSecondary = () => {
             <Box position="absolute" left="0px" top="400px">
                 <LeftShadow />
             </Box>
-            <Container maxWidth="lg" sx={{ height: isMobile ? '120vh' : '70vh', display: 'flex', justifyContent: 'center', alignItems: 'center',marginTop:'8rem' }}>
-                <Box my={'4rem'} >
-                    <Flex flexDirection="column" gap="3rem">
-                        <Flex flexDirection={{ sm: 'row', xs: 'column' }} gap={{ sm: '1rem', xs: '0rem' }}>
-                            <Heading color={DEFAULT_COLORS.White} lineHeight={isMobile ? '35px' : '55px'}>What Makes us </Heading>
-                            <Heading color={DEFAULT_COLORS.Blue} lineHeight={isMobile ? '35px' : '55px'}>Better</Heading>
+            <MainRelativeContainer>
+                {/* <LeftMaskContainer>
+                    <img src="Images/blueleft.png"></img>
+                </LeftMaskContainer>
+                <RightMaskContainer>
+                    <img src="Images/blueright.png"></img>
+                </RightMaskContainer> */}
+                <Container maxWidth="lg" sx={{ height: isMobile ? '120vh' : '70vh', display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '8rem' }}>
+                    <Box my={'4rem'} >
+                        <Flex flexDirection="column" gap="3rem">
+                            <Flex flexDirection={{ sm: 'row', xs: 'column' }} gap={{ sm: '1rem', xs: '0rem' }}>
+                                <Heading color={DEFAULT_COLORS.White} lineHeight={isMobile ? '35px' : '55px'}>What Makes us </Heading>
+                                <Heading color={DEFAULT_COLORS.Blue} lineHeight={isMobile ? '35px' : '55px'}>Better</Heading>
+                            </Flex>
+                            <Text color={DEFAULT_COLORS.Light} maxWidth="300px" textAlign="center" lineHeight="25px" fontSize="17px">With a seamless and secure platform, Nexus is redefining the excellence of launches.</Text>
+                            <Flex style={{ width: '100%' }} flexDirection={{ sm: 'row', xs: 'column' }} >
+                                <SecondayCard image={IMAGE_COLLECTIONS.NexusN} description="Community backed launches infused into an innovative platform." />
+                                <SecondayCard image={IMAGE_COLLECTIONS.Hammer} description="Premium experience on all devices with user-friendly interface." />
+                                <SecondayCard image={IMAGE_COLLECTIONS.Settings} description="Perfectly crafted tools for all new launchpad experience." />
+                            </Flex>
                         </Flex>
-                        <Text color={DEFAULT_COLORS.Light} maxWidth="300px" textAlign="center" lineHeight="25px" fontSize="17px">With a seamless and secure platform, Nexus is redefining the excellence of launches.</Text>
-                        <Flex style={{ width: '100%' }} flexDirection={{ sm: 'row', xs: 'column' }} >
-                            <SecondayCard image={IMAGE_COLLECTIONS.NexusN} description="Community backed launches infused into an innovative platform." />
-                            <SecondayCard image={IMAGE_COLLECTIONS.Hammer} description="Premium experience on all devices with user-friendly interface." />
-                            <SecondayCard image={IMAGE_COLLECTIONS.Settings} description="Perfectly crafted tools for all new launchpad experience." />
-                        </Flex>
-                    </Flex>
-                </Box>
-            </Container>
+                    </Box>
+                </Container>
+            </MainRelativeContainer>
+
         </>
     )
 }
